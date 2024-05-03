6e57776db64419bb259f9984824f50cc902f3438
Jose Henrique <jh4832184@gmail.com>
2024-05-03 16:10:17 -0300

    refactor: customizing button with icons and refactoring Navbar with this button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,23 @@
-interface buttonProps {
-  onclick: ()=>void,
-  children: string
+import { ButtonHTMLAttributes, ReactNode } from "react"
+
+interface buttonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: string,
+  children?: ReactNode
 }
-export function Button({onclick, children}: buttonProps) {
+export function Button({icon, children, ...props}: buttonProps) {
   return (
     <button
-      onClick={onclick}
+      {...props}
       className="
+        flex items-center justify-center text-lg
         bg-blue-800 text-white 
-        hover:bg-blue-700 active:bg-blue-900 active:shadow-gray-300
-        duration-300  rounded     active:shadow-md
-        px-2 py-1 w-24"
-    >{children}</button>
+        hover:bg-blue-700 active:bg-blue-900 active:shadow-gray-300 active:shadow-md
+        duration-300 rounded gap-1 px-1 w-auto">
+
+      {icon && <img
+                  className="size-5"
+                  src={`/icons/${icon}.svg`} alt={`icon ${icon}`} />}
+      {children}
+    </button>
   )
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,32 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
-import { ButtonAuth } from "../ButtonAuth";
 import { ButtonMenu } from "../ButtonMenu";
 import { useRef} from "react";
+import { Button } from "../Button";
 
 export function NavBar() {
-
-  const token = Cookies.get("token");
   const navLinks = useRef<HTMLDivElement>(null);
+  const navigate  = useNavigate();
+  const token = Cookies.get("token");
 
-  /* Verifa se a div tem flex depois troca pra hidden e vice versa */
+  // faz logout ou apenas o redirecionamento para a pagina de login
+  function handleAuthClick () {
+    if (token) {Cookies.remove("token")}
+    navigate("/login")
+  }
+
+  // Verifa se a div tem flex depois troca pra hidden e vice versa
   function handleMenuClick () {
     navLinks.current?.classList.replace("flex", "hidden")
     ||
     navLinks.current?.classList.replace("hidden", "flex"); 
   }
 
+  const nameAuth = token? "Logout": "Enter";
+  const iconAuth = token? "logout": "login"
   return (
     <nav className="flex flex-col p-2 gap-2 border-b shadow-sm sm:flex-row sm:justify-between">
       <div className="flex justify-between sm:order-2">
-          <ButtonMenu handleMenuClick={handleMenuClick} />
-          <ButtonAuth token={token}/>
+          <ButtonMenu 
+            handleMenuClick={handleMenuClick} />
+          <Button 
+            onClick={handleAuthClick} 
+            icon={iconAuth}
+          >{nameAuth}</Button>
       </div>
       <div ref={navLinks} className="hidden flex-col sm:flex sm:flex-row sm:order-1 sm:gap-2">
           <Link to="/">Home</Link>
           {
             token && <>
-              <Link to="/">Dasboard</Link>
+              <Link to="/dashboard">Dasboard</Link>
               <Link to="/">Profile</Link>
             </>
           }
