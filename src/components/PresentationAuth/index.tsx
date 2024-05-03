fd3d46c60a020273555e4774193ffef3dfa6cf4a
Jose Henrique <jh4832184@gmail.com>
2024-05-03 11:46:00 -0300

    view: add PresentationAuth

---
+++ src/components/PresentationAuth/index.tsx
@@ -0,0 +1,10 @@
+export function PresentationAuth() {
+    return ( 
+        <div className="space-y-0.5 ">
+            <h1
+            className="font-semibold text-xl text-blue-800 text-center"
+            >TASK BLOCK</h1>
+            <p>Welcome to app of task manager</p>
+        </div>
+     );
+}
