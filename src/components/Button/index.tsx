b1a358e4d2da04c5c12c4af0e47f83f5b90fbf41
Jose Henrique <jh4832184@gmail.com>
2024-05-04 09:34:18 -0300

    refactor: add twMerge to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,25 @@
 import { ButtonHTMLAttributes, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 interface buttonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: string,
   children?: ReactNode
 }
-export function Button({icon, children, ...props}: buttonProps) {
+export function Button({icon, children, className, ...props}: buttonProps) {
   return (
     <button
       {...props}
-      className="
-        flex items-center justify-center text-lg
+      className={twMerge(
+        `flex items-center justify-center w-auto gap-1 px-1   
         bg-blue-800 text-white 
-        hover:bg-blue-700 active:bg-blue-900 active:shadow-gray-300 active:shadow-md
-        duration-300 rounded gap-1 px-1 w-auto">
-
-      {icon && <img
-                  className="size-5"
-                  src={`/icons/${icon}.svg`} alt={`icon ${icon}`} />}
+        hover:bg-blue-700 active:bg-blue-900 active:shadow-gray-300 active:shadow-md 
+        duration-300 rounded text-lg`,
+        className
+      )}>
+        
+      {icon && 
+      <img className="size-5" src={`/icons/${icon}.svg`} alt={`icon ${icon}`} />}
+      
       {children}
     </button>
   )
