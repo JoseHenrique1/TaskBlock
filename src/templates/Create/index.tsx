a1bcb2ffdbb6f10f24f9cb882b42d3d7fabf91d1
Jose Henrique <jh4832184@gmail.com>
2024-05-08 16:31:34 -0300

    feat: add handleSubmit, check token and states

--- src/templates/Create/index.tsx
+++ src/templates/Create/index.tsx
@@ -1,13 +1,46 @@
 import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
+
 import { Button } from "../../components/Button";
 import { FormField } from "../../components/FormField";
+import { ColorDropdown } from "../../components/ColorDropdown";
+
+
+const API = import.meta.env.VITE_API;
 
 export function CreateTask() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [favorite, setFavorite] = useState(false);
+    const [color, setColor] = useState("blue");
     const navigate = useNavigate();
+
+    useEffect(()=>{
+        const token = Cookies.get("token");
+        if (!token) {
+          navigate("/login");
+          return;
+        }
+    }, []);
+
     function handleSubmit (e:React.FormEvent<HTMLFormElement>) {
+        const token = Cookies.get("token")
         e.preventDefault();
-        alert("ok submit");
-        navigate("/dashboard");
+        fetch(API+"tasks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=UTF-8",
+                "token": token!
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                isFavorite: favorite,
+                colorBackground: color
+            })
+        });
+        navigate("/dashboard", {replace: true});
     }
 
     return ( 
@@ -17,16 +50,25 @@ export function CreateTask() {
                     <div className="flex gap-2 self-end">
                         <Button title="Save" type="submit" icon="save" className="p-1"/>
                         <Button title="Reset all changes" type="reset" icon="reset" className="p-1"/>
-                        <Button title="Change color" icon="pallete" className="p-1"/>
-                        <Button title="Favorite" icon="star" className="p-1"/>
+                        <ColorDropdown setColor={setColor}/>
+                        <Button 
+                            onClick={()=>{setFavorite(prev=>!prev)}}
+                            type="button"
+                            title="Favorite" 
+                            icon={favorite? "star_marked": "star"} 
+                            className="p-1"/>
                     </div>
 
                     <FormField 
+                        value={title}
+                        onChange={(e)=>{setTitle(e.target.value)}}
                         type="text" 
                         className="flex-grow py-0"
                         placeholder="Title" />
                 </div>
                 <textarea
+                    value={description}
+                    onChange={(e)=>{setDescription(e.target.value)}}
                     className=" flex-grow border rounded resize-none p-2"  
                     placeholder="Description"/>
             </form>
