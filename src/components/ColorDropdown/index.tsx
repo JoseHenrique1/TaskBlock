3481f5389926d2407e44f940499e95f8e747c478
Jose Henrique <jh4832184@gmail.com>
2024-05-08 16:10:09 -0300

    view: add colorDropdown

---
+++ src/components/ColorDropdown/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { Button } from "../Button";
+
+interface colorDropdownProps {
+    setColor: (color: string)=>void
+}
+
+export function ColorDropdown({setColor}: colorDropdownProps) {
+    const [show, setShow] = useState(false);
+
+    function handleShowGridColor () {
+        setShow(prev=>!prev)
+    }
+
+    function handleSetColor(color: string) {
+        setColor(color);
+        handleShowGridColor();
+    }
+
+
+    return ( 
+        <div className="relative">
+            <Button 
+                type="button"
+                onClick={handleShowGridColor} 
+                className="w-full h-full"
+                icon="pallete"/>
+            {show &&
+                <div className="absolute -left-16 bg-zinc-200 grid grid-cols-2 grid-rows-2 gap-2 p-2 w-32 h-32">
+                    <button type="button" className="bg-red-600 " onClick={()=>handleSetColor("red")}/>
+                    <button type="button" className="bg-blue-600 " onClick={()=>handleSetColor("blue")}/>
+                    <button type="button" className="bg-green-600 " onClick={()=>handleSetColor("green")}/>
+                    <button type="button" className="bg-orange-600  " onClick={()=>handleSetColor("orange")}/>
+                </div>
+            }
+        </div>
+    );
+}
