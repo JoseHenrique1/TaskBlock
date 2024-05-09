1d10524526a69a0f52fd0674d1e095762d80481e
Jose Henrique <jh4832184@gmail.com>
2024-05-09 10:07:32 -0300

    refactor: using taskcontext in container cart

--- src/components/ContainerCart/index.tsx
+++ src/components/ContainerCart/index.tsx
@@ -1,45 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect} from "react";
+import { taskContext } from "../../contexts/task";
 import { Cart } from "../Cart";
-import Cookies from "js-cookie";
-
-const API = import.meta.env.VITE_API;
-
-interface taskInterface {
-    id: string,
-    title: string,
-    description?: string,
-    isFavorite: boolean,
-    colorBackground: string,
-    userId: string
-}
-
-interface getDataInterface {
-    statusCode: number,
-    tasks: Array<taskInterface>
-}
 
 export function ContainerCart() {
-    const token = Cookies.get("token");
-    const [tasks, setTasks] = useState<Array<taskInterface>>([])
+    const {tasks,handleGetTasks} = useContext(taskContext);
 
-    function handleGetData () {
-        fetch(API+"tasks", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "token": token!
-            }
-        })
-        .then(data=>data.json())
-        .then((data: getDataInterface)=>{
-            if (data.statusCode == 200) {
-                setTasks(data.tasks);      
-            }
-        })
-        .catch(error=>console.log(error))
-    }
-
-    useEffect(handleGetData, [])
+    useEffect(handleGetTasks, [])
 
     return ( 
         <div className="w-full grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-3  max-w-screen-xl lg:mx-auto lg:my-0">
@@ -47,10 +13,3 @@ export function ContainerCart() {
         </div>
      );
 }
-
-
-
-
-
-
-
