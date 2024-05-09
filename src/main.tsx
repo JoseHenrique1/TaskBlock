43522c4ef60aae917e3779e7bcaf286ba13b8b05
Jose Henrique <jh4832184@gmail.com>
2024-05-09 10:05:41 -0300

    feat: add taskprovider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter} from 'react-router-dom';
 import './globals.css';
 
 import { AlertProvider } from './contexts/alert/index.tsx';
+import { TaskProvider } from './contexts/task/index.tsx';
 
 import { NavBar } from './components/NavBar/index.tsx';
 import { Footer } from './components/Footer/index.tsx';
@@ -11,11 +12,13 @@ import { Router } from './router.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <AlertProvider>
-    <div className='flex flex-col min-h-screen'>
-      <NavBar/>
-      <Router/>
-      <Footer/>
-    </div>
+    <TaskProvider>
+      <div className='flex flex-col min-h-screen'>
+        <NavBar/>
+        <Router/>
+        <Footer/>
+      </div>
+    </TaskProvider>
     </AlertProvider>
   </BrowserRouter>
 );
