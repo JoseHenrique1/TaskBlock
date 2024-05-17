1f27c6b3fedef75b4026b667ca1e487a73c78292
Jose Henrique <jh4832184@gmail.com>
2024-05-17 07:17:36 -0300

    feat: add filter cards

--- src/components/ContainerCard/index.tsx
+++ src/components/ContainerCard/index.tsx
@@ -2,14 +2,23 @@ import { useContext, useEffect} from "react";
 import { taskContext } from "../../contexts/task";
 import { Card } from "../Card";
 
-export function ContainerCard() {
+interface containerCardProps {
+    search: string
+}
+export function ContainerCard({search}: containerCardProps) {
     const {tasks,getTasks} = useContext(taskContext);
 
     useEffect(getTasks, [])
 
+    let taskFiltered = tasks.filter(item=>item.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+
     return ( 
         <div className="w-full grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-3  max-w-screen-xl lg:mx-auto lg:my-0">
-            {tasks.map(task => <Card key={task.id} task={task} />)}
+            {taskFiltered.length !== 0 ?
+                taskFiltered.map(task => <Card key={task.id} task={task} />)
+                :
+                <p className="sm:col-span-2 md:col-span-3 lg:col-span-4">No task was found with the name "{search}"</p>
+            }
         </div>
      );
 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,9 +3,11 @@ import { FormField } from "../../components/FormField";
 import { ContainerCard } from "../../components/ContainerCard";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function Dashboard() {
+    const [search, setSearch] = useState("")
+
     const navigate = useNavigate();
 
     function handleCreateTask () {
@@ -23,10 +25,10 @@ export function Dashboard() {
     return ( 
         <main className="flex-grow px-2 space-y-2 mt-2">
             <div className="flex flex-col-reverse gap-2 items-center sm:flex-row sm:justify-between">
-                <FormField type="text" placeholder="Search for your tasks" />
+                <FormField value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="Search for your tasks" />
                 <Button onClick={handleCreateTask} icon="plus">New task</Button>
             </div>
-            <ContainerCard/>
+            <ContainerCard search={search}/>
         </main> 
     );
 }
