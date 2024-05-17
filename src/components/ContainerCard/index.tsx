473a93f93493629d26f126c226c6b33708fb2613
Jose Henrique <jh4832184@gmail.com>
2024-05-17 09:14:40 -0300

    fix: fixing logic for to filter tasks

--- src/components/ContainerCard/index.tsx
+++ src/components/ContainerCard/index.tsx
@@ -14,10 +14,10 @@ export function ContainerCard({search}: containerCardProps) {
 
     return ( 
         <div className="w-full grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-3  max-w-screen-xl lg:mx-auto lg:my-0">
-            {taskFiltered.length !== 0 ?
+            {tasks.length !== 0 ?
                 taskFiltered.map(task => <Card key={task.id} task={task} />)
                 :
-                <p className="sm:col-span-2 md:col-span-3 lg:col-span-4">No task was found with the name "{search}"</p>
+                <p className="sm:col-span-2 md:col-span-3 lg:col-span-4">Create your first task!</p>
             }
         </div>
      );
